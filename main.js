9c2ebbc5809bdcd982bbe10c1b4006daf8f1df1a
Gabriel-Dias <gabrieldias_02@hotmail.com>
2024-01-07 21:16:58 -0300

    att gerais e implementação do javascript

---
+++ main.js
@@ -0,0 +1,13 @@
+
+function validarForm() {
+  let emailInput = document.querySelector("#emailClient");
+  let checkboxInput = document.querySelector("#accept");
+
+  if (checkboxInput.checked) {
+    alert("Agora ficará por dentro das ofertas atráves do seu e-mail: " + emailInput.value);
+  } else {
+    alert(
+      "Por favor, concorde com os termos e condições antes de enviar o e-mail."
+    );
+  }
+}
