11787da905d1616956e66dc76f2446cc5ae6e03b
Gabriel-Dias <gabrieldias_02@hotmail.com>
2024-01-10 17:14:59 -0300

    menu att

--- main.js
+++ main.js
@@ -4,16 +4,20 @@ function abrirMenu() {
   mostrarMenu.style.top = "0"
 }
 
+function fecharMenu() {
+  let mostrarMenu = document.querySelector(".menuAberto");
+  mostrarMenu.style.top = "-100%";
+}
+
 function validarForm() {
   let emailInput = document.querySelector("#emailClient");
   let checkboxInput = document.querySelector("#accept");
 
   if (checkboxInput.checked) {
     alert("Agora ficará por dentro das ofertas atráves do seu e-mail: " + emailInput.value);
-  } else {
-    alert(
+  } alert(
       "Por favor, concorde com os termos e condições antes de enviar o e-mail."
     );
-  }
+  
 }
 
